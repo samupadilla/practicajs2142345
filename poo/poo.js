7ff14b2c367e251b8b77edd473835f84e0ae091d
srpadilla <srpadilla@misena.edu.co>
2021-10-01 16:25:03 -0500

    Explico POO en Javascript

---
+++ poo/poo.js
@@ -0,0 +1,85 @@
+//4ta forma de crear objetos en JS
+class Person{    
+    constructor(nombre,apellido){
+        this.nombre=nombre;
+        this.apellido=apellido;
+    }
+    miNombreCompleto(){
+        return this.nombre+' '+this.apellido;
+    }
+}
+
+var p1=new Person('Andrea','Valdiri');
+console.log(p1.miNombreCompleto());
+var p3=new Person();
+p3.nombre='Viviana';
+p3.apellido='Gonzalez';
+console.log(p3.miNombreCompleto());
+//3ra forma para crear objetos en JS
+
+//var plantilla=function(){//No tengo acceso a los datos
+function Persona(nombre,apellido){
+    this.nombre=nombre;
+    this.apellido=apellido;
+    this.nombreCompleto=function(){
+        return this.nombre+' '+this.apellido;
+    }
+}
+var obfunction=new Persona('alex','castro');
+var o1=new Persona('jhon','suarez');
+console.log(o1.nombre);
+var o2=new Persona('Gladys','Merchan');
+var o3=new Persona('Valentina','Mor');
+console.log(typeof(o3));
+console.log(obfunction.nombre);
+console.log(obfunction.nombreCompleto());
+
+
+
+
+
+//2da forma para crear objetos en JS
+//JSON formato clave:valor
+var obj={
+ nombre:'Luis',
+ apellido:'Cifuentes',
+ nombreCompleto:function(){
+     return this.nombre+' '+this.apellido;
+ }
+};
+console.log(obj.nombre);
+console.log(obj['nombre']);
+console.log(obj.apellido);
+console.log(obj.nombreCompleto());
+obj.telefono='301333333';
+console.log(obj);
+delete obj.apellido;
+console.log(obj);
+obj.nombre='Juan Luis';
+console.log(obj)
+
+
+//1ra forma de crear objetos en JS
+//this= esta clase o este objeto
+var ob=new Object();
+ob.nombre='Alfonso';
+ob.apellido='Lizarazo';
+ob.nombreCompleto=function(){
+    return this.nombre+' '+this.apellido;
+}
+console.log(ob.nombreCompleto())
+
+
+
+
+
+
+//tipos de dato
+ var x=3;
+ console.log(typeof(x));
+// var fecha=new Date();
+// console.log(typeof(fecha));
+// console.log(fecha);
+// var z='hola';
+// console.log(typeof(z));
+// console.log(z);
--- relacionjs.js
+++ relacionjs.js
@@ -24,24 +24,34 @@ console.log(sumaPares(vec));
 //hoisting
 function llenarVector(vector, tam) {
   for (let i = 0; i < tam; i++) {
-    vector.push(Math.round(Math.random() * 100));
+    vector.push(Math.round(Math.random() * 10));
   }
   return vector;
 }
 
 var llenar = (vector, tam) => {
   for (let i = 0; i < tam; i++) {
-    vector.push(Math.round(Math.random() * 100));
+    vector.push(Math.round(Math.random() * 10));
   }
   return vector;
-}
-v=new Array();
+};
+v = new Array();
 
-console.log(llenar(v,7));
+console.log(llenar(v, 5));
 
+var suma = (vector) => {
+  var s = 0;
+  for (let i = 0; i < vector.length; i++) {
+    s = s + vector[i];
+    console.log("aqui si puedo usar i: " + i);
+  }
+  return s;
+};
+
+console.log("Suma del vector: " + suma(v));
 
 function sumaPares(vector) {
-  suma = 0;
+  let suma = 0;
   for (let i = 0; i < vector.length; i++) {
     if (vector[i] % 2 == 0) {
       suma += vector[i];
